47a514a1976eb8a15a033b8235280004fb3f19b1
travis-wentz <wentz.travis@gmail.com>
2015-10-31 14:51:28 -0600

    fcfs and averages
    first come first serve done and averages added to stats

--- wentzHW03/Driver.java
+++ wentzHW03/Driver.java
@@ -3,7 +3,25 @@ package wentzHW03;
 public class Driver {
 	public static void main(String[] args) {
 		int[][] requests = new int[50][3];
+		int[][] hisRequests = {
+				{0, 54, 0},
+				{23, 132, 6},
+				{26, 29, 2},
+				{29, 23, 1},
+				{35, 198, 7},
+				{45, 170, 5},
+				{57, 180, 3},
+				{83, 78, 4},
+				{88, 73, 5},
+				{95, 249, 7}
+		};
+		//HardDriveSim.print(hisRequests);
+		
+		System.out.println("--------------------------");
+		
+		HardDriveSim.fCFS(hisRequests);
+		
 		requests = HardDriveSim.createRandomRequests(50);
-		HardDriveSim.print(requests);
+		//HardDriveSim.print(requests);
 	}
 }
--- wentzHW03/HardDriveSim.java
+++ wentzHW03/HardDriveSim.java
@@ -1,6 +1,48 @@
 package wentzHW03;
 
+/*
+ * Simulates different algorithms for servicing requests. Requests are in the form of a multi-dimensional array
+ * where the first column is arrival time, second col is track requested, third col is sector requested.
+ * Given: 250 tracks; 8 sectors (0-7) per track; start position is 0, 0; only rotates 1 direction and takes 1ms to
+ * move to next sector; seek time is 10 +0.1*T ms where T = number of tracks between previous and current.
+ */
 public class HardDriveSim {
+	
+	/*
+	 * First-come-first-serve algorithm.
+	 * Serves requests in order of arrival time.
+	 * 
+	 */
+	protected static void fCFS(int[][] requests){
+		int curTrack = 0;
+		int curSector = 0;
+		double T = 0;
+		double turnaround = 0;
+		double finishTime = 0;
+		int nextSector = 0;
+		double[][] times = new double[requests.length][2]; //stores turnaround time and finish time for each request
+		
+		for(int i = 0; i < requests.length; i++){
+			//find time to next track
+			T = Math.abs(curTrack - requests[i][1]);
+			turnaround = 10 + 0.1 * T;
+			//find time to next sector
+			nextSector = requests[i][2];
+			if(nextSector > curSector){
+				turnaround += nextSector - curSector;
+			} else if (nextSector < curSector){
+				turnaround += 8 - curSector + nextSector;
+			}
+			finishTime += turnaround;
+			times[i][0] = turnaround;
+			times[i][1] = finishTime;
+			curTrack = requests[i][1];
+			curSector = requests[i][2];
+		}
+		
+		stats("FCFS", times);
+	}
+	
 	/*
 	 * creates random requests and adds them to the array in order of arrival time
 	 */
@@ -49,5 +91,19 @@ public class HardDriveSim {
 			System.out.println();
 		}
 		//System.out.println("-------------------------------");
+	}
+	
+	protected static void stats(String name, double[][] times){
+		double avgTurnaround = 0;
+		double avgFinish = 0;
+		for(int i = 0; i < times.length; i++){
+			avgTurnaround += times[i][0];
+			avgFinish += times[i][1];
+		}
+		avgTurnaround /= times.length;
+		avgFinish /= times.length;
+		
+		System.out.println(name + " average turnaround time: " + avgTurnaround);
+		System.out.println(name + " average finish time: " + avgFinish);
 	}
 }
